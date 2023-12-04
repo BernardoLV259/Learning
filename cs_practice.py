0b7bf68b707d9fe22ddaf13e8c1762a22df33391
Bernardo Vasquez <130615812+BernardoLV259@users.noreply.github.com>
2023-12-03 21:18:06 -0500

    Add files via upload

---
+++ cs_practice.py
@@ -0,0 +1,13 @@
+def init_mat(value):
+    list = []
+    
+    for num in range(value):
+        new_list = []
+        
+        for i in range(value):
+            new_list.append(value)
+        
+        list.append(new_list)
+    return list
+
+
